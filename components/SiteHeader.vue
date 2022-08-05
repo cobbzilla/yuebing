d977b8f2e5dc253ec7bb3c91765480214c901ee2
Jonathan Cobb <jonathan@kyuss.org>
2022-08-05 09:40:57 -0400

    add default layout with simple header/footer

---
+++ components/SiteHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    This is the header
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <SiteHeader/>
+    <Nuxt />
+    <SiteFooter/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
