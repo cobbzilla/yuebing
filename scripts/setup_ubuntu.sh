707c10098d3e34f438ceaf061de91fb6cb380c1d
Jonathan Cobb <jonathan@kyuss.org>
2022-08-14 18:24:54 -0400

    run docker-compose build/up

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y && sudo apt install -y docker.io docker-compose git 
+sudo apt update && sudo apt upgrade -y && sudo apt install -y docker.io docker-compose git && \
+git clone https://github.com/cobbzilla/yuebing.git && \
+cd yuebing && docker-compose build && \
+docker-compose up -d
