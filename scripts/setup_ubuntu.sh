b37a3a5e0d922f0dd3e3db8f29c0d88c75cd4a07
Jonathan Cobb <jonathan@kyuss.org>
2022-09-21 15:26:59 -0400

    adjust commands, use noninteractive

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -14,20 +14,20 @@ if [[ -n "${LE_EMAIL}" && -n "${LE_HOSTNAME}" ]] ; then
   sudo bash -c "echo -n \"${LE_HOSTNAME}\" > /etc/hostname"
 fi
 
-sudo apt update && \
-  sudo apt upgrade -y && \
-  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && \
+sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -y update' && \
+  sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -y upgrade' && \
+  sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt install apt-transport-https ca-certificates curl software-properties-common -y' && \
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
-  sudo apt update && \
-  sudo apt install docker-ce -y && \
+  sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -y update' && \
+  sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt install docker-ce -y' && \
   sudo -E usermod -aG docker "${THIS_USER}"
 
 if [[ -n "${LE_EMAIL}" && -n "${LE_HOSTNAME}" ]] ; then
-  sudo apt install certbot -y
-  if [[ $(find /etc/letsencrypt/accounts -type f -name regr.json | xargs grep -l "${LE_EMAIL}" | wc -l | tr -d ' ') -eq 0 ]] ; then
+  sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt install certbot -y'
+  if [[ $(sudo bash -c 'find /etc/letsencrypt/accounts -type f -name regr.json | xargs grep -l '"${LE_EMAIL}"' | wc -l | tr -d " " ') -eq 0 ]] ; then
     echo "certbot register starting: certbot register --agree-tos -m ${LE_EMAIL} --non-interactive"
-    certbot register --agree-tos -m "${LE_EMAIL}" --non-interactive
+    sudo bash -c 'certbot register --agree-tos -m '"${LE_EMAIL}"' --non-interactive'
     OK=$?
     echo "certbot register completed, OK=${OK}"
     if [[ ${OK} -ne 0 ]] ; then
@@ -35,24 +35,24 @@ if [[ -n "${LE_EMAIL}" && -n "${LE_HOSTNAME}" ]] ; then
     fi
   fi
   CERT_FILE=/etc/letsencrypt/live/"${LE_HOSTNAME}"/privkey.pem
-  if [[ -f "${CERT_FILE}" ]] ; then
+  if [[ $(sudo bash -c 'ls -l "'"${CERT_FILE}"'"' 2> /dev/null | wc -l | tr -d ' ') -gt 0 ]] ; then
     echo "cert exists: ${CERT_FILE}"
   else
-    certbot certonly --standalone -d "${LE_HOSTNAME}"
+    sudo bash -c 'certbot certonly --standalone -d '"${LE_HOSTNAME}"
   fi
 fi
 
 if [[ -n "${MOUNT_PATH}" ]] ; then
   sudo mkdir -p "${MOUNT_PATH}"
-  mount "${MOUNT_DEVICE}" "${MOUNT_PATH}" || \
-    mkfs -t xfs "${MOUNT_DEVICE}" && \
-    mount "${MOUNT_DEVICE}" "${MOUNT_PATH}"
+  sudo mount "${MOUNT_DEVICE}" "${MOUNT_PATH}" || \
+    sudo mkfs -t xfs "${MOUNT_DEVICE}" && \
+    sudo mount "${MOUNT_DEVICE}" "${MOUNT_PATH}"
   if [[ "${MOVE_DOCKER_TO_MOUNT}" != "NO" ]] ; then
-    mkdir -p "${MOUNT_PATH}"/docker
-    mkdir -p /etc/docker
-    service docker stop
-    echo "{\"data-root\": \"${MOUNT_PATH}/docker\"}" > /etc/docker/daemon.json
-    service docker start
+    sudo mkdir -p "${MOUNT_PATH}"/docker
+    sudo mkdir -p /etc/docker
+    sudo service docker stop
+    sudo bash -c 'echo "{\"data-root\": \"'"${MOUNT_PATH}"'/docker\"}" > /etc/docker/daemon.json'
+    sudo service docker start
   fi
 fi
 
